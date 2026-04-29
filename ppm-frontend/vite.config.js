a53c38ba0885d12758fe3522598603fec66aed56
silenceafter <a59-info@yandex.ru>
2026-04-30 00:07:28 +0300

    30.04.2026

--- ppm-frontend/vite.config.js
+++ ppm-frontend/vite.config.js
@@ -12,7 +12,7 @@ const baseFolder =
         ? `${env.APPDATA}/ASP.NET/https`
         : `${env.HOME}/.aspnet/https`;
 
-const certificateName = "production-process-management-app.client";
+const certificateName = "ppm-frontend";
 const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
 const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
 
